a4ea0ff50ea2f470b838bc3bece78a943a8f1fd9
DeffOS <68462654+DeffOS@users.noreply.github.com>
2024-11-21 01:13:03 +0300

    Main Commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+window.onload = function () {
+	$("#serverName").text( "ASS" );
+}
+
+function GameDetails(servername, serverurl, mapname, maxplayers, steamid, gamemode, volume, lang, gamemodeNice) {
+	$("#serverName").text( "ASS" );
+
+}
+
+function DownloadingFile(file) {
+}
+
+function SetStatusChanged(status) {
+}
